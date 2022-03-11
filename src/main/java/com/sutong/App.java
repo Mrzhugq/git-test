73fa7df56c0bfe5d2619cfd6d4cf0f98e3167cbf
Mrzhugq <84071063+Mrzhugq@users.noreply.github.com>
2022-03-11 18:05:04 +0800

    pull-test-commit

--- src/main/java/com/sutong/App.java
+++ src/main/java/com/sutong/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello World! 2222" );
         System.out.println( "hot-fix 3333 " );
         System.out.println( "test push" );
+        System.out.println( "test pull" );
     }
 }
