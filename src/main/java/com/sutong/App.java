6c533aa3496c497918fc695d151d82864c937b05
sutong <2081140153@qq.com>
2022-03-11 17:52:34 +0800

    test-push-commit-1

--- src/main/java/com/sutong/App.java
+++ src/main/java/com/sutong/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! 2222" );
         System.out.println( "hot-fix 3333 " );
+        System.out.println( "test push" );
     }
 }
